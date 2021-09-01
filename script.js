376056d8d0c40913272c0481b5a5bc3106813db6
ResminiLoris <resminiloris@gmail.com>
2021-09-01 18:33:03 +0200

    stampo_1_mail

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ console.log("vue", Vue);
 
 Vue.config.devtools = true;
 
-cost app = new Vue({
+const app = new Vue({
     el: "#app",
-    data:{},
-    methods:{},
+    data:{
+        mail:"",
+    },
+    methods:{
+
+    },
+    created(){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+            console.log(res.data.response);
+            this.mail = res.data.response;
+            this.mailList.push(this.mail);
+        })
+    }
 });
